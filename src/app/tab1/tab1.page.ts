d37ab870ee7ae26d7373a439c778e969b8cc4d6c
sebafra <seba@diproach.com>
2022-06-23 12:20:43 -0300

    Markets page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  items: any;
+  url: any = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+  info: any = {};
+  loading: any;
 
-  constructor() {}
+  constructor(
+    public loadingController: LoadingController
+  ) {
+    this.getItems();
+  }
 
+  getItems(){
+    this.presentLoading();
+    fetch(this.url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.info = data;
+      })
+      .catch(error => { console.error(error); });
+  }
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Connecting with coinbase...',
+      duration: 1500
+    });
+    await this.loading.present();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  items: any = {};
+  url: any = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
+  loading: any;
 
-  constructor() {}
+  constructor(
+    public loadingController: LoadingController
+  ) {
+    this.getItems();
+  }
+
+  getItems() {
+    this.presentLoading();
+    fetch(this.url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.items = data;
+      })
+      .catch(error => { console.error(error); });
+  }
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Connecting with coindesk...',
+      duration: 1500
+    });
+    await this.loading.present();
+  }
+
 
 }
